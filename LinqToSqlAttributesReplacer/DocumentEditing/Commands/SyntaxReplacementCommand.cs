9450075a773af4871420bba4ac82a9b0796a7fc5
Alexey Ivanov <alexei.nt@gmail.com>
2016-04-30 09:19:30 +0500

    Implementation of SyntaxReplacementCommand

---
+++ LinqToSqlAttributesReplacer/DocumentEditing/Commands/SyntaxReplacementCommand.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace LinqToSqlAttributesReplacer.DocumentEditing.Commands
+{
+    public class SyntaxReplacementCommand : IDocumentEditingCommand
+    {
+        private readonly SyntaxNode nodeForReplacement;
+        private readonly SyntaxNode newNode;
+
+        public SyntaxReplacementCommand(SyntaxNode nodeForReplacement, SyntaxNode newNode)
+        {
+            this.nodeForReplacement = nodeForReplacement;
+            this.newNode = newNode;
+        }
+
+        public DocumentEditor Execute(DocumentEditor documentEditor)
+        {
+            documentEditor.ReplaceNode(nodeForReplacement, newNode);
+            return documentEditor;
+        }
+    }
+}
