a0881c1d6166952584b543850a4c90582559689b
Alexey Ivanov <alexei.nt@gmail.com>
2016-04-30 13:55:04 +0500

    Document processor optimization

--- LinqToSqlAttributesReplacer/DocumentProcessor.cs
+++ LinqToSqlAttributesReplacer/DocumentProcessor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LinqToSqlAttributesCommon.Interfaces;
@@ -22,15 +23,17 @@ namespace LinqToSqlAttributesReplacer
 
         public async Task ProcessAsync(Document document)
         {
-            var documentSyntax = await document.GetSyntaxRootAsync().ConfigureAwait(false);
-            var documentEditingCommands = documentEditingCommandsCompositeFactory.Create(documentSyntax);
+            var documentSyntaxTask = document.GetSyntaxRootAsync();
+            var documentEditorTask = DocumentEditor.CreateAsync(document);
 
+            var documentSyntax = await documentSyntaxTask.ConfigureAwait(false);
+            var documentEditingCommands = documentEditingCommandsCompositeFactory.Create(documentSyntax);
             if (documentEditingCommands.Length == 0)
             {
                 return;
             }
 
-            var documentEditor = await DocumentEditor.CreateAsync(document).ConfigureAwait(false);
+            var documentEditor = await documentEditorTask.ConfigureAwait(false);
             documentEditingCommands.Aggregate(documentEditor, (editor, command) => command.Execute(documentEditor));
             var updatedDocument = documentEditor.GetChangedDocument();
 
