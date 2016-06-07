4c5fce7cb1e1aef84c3d0f703fb6111019ca3a75
a.ivanov <a.ivanov@skbkontur.ru>
2016-06-07 19:26:35 +0500

    Fix not implemented in DiscriminatorAttributeParameterConverter

--- LinqToSqlAttributesReplacer/DocumentEditing/Factories/PropertyAttributeConverters/DiscriminatorAttributeParameterConverter.cs
+++ LinqToSqlAttributesReplacer/DocumentEditing/Factories/PropertyAttributeConverters/DiscriminatorAttributeParameterConverter.cs
@@ -3,14 +3,13 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LinqToSqlAttributesReplacer.DocumentEditing.Factories.PropertyAttributeConverters
 {
-    //TODO: Implement (Right)
     public class DiscriminatorAttributeParameterConverter : IAttributeParameterConverter
     {
         private const string columnIsDescriminator = AttributeArgumentName.ColumnIsDescriminator;
 
         public AttributeSyntax Convert(AttributeSyntax attribute)
         {
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
